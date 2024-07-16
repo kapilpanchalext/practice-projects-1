c895ce22413863ba77f7e377d090e2499b4a8c12
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-16 10:10:00 +0530

    Merge branch 'v1.0.0' of https://github.com/kapilpanchalext/practice-projects-1.git into v1.0.0

--- richtext-editor-pdf-download-1/src/main/java/com/app/pdf/api/PDFController.java
+++ richtext-editor-pdf-download-1/src/main/java/com/app/pdf/api/PDFController.java
@@ -2,6 +2,7 @@ package com.app.pdf.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PDFController {
 	
 	@GetMapping(path = "/hello")
