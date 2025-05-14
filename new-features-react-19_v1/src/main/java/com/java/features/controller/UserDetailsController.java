161b1dbffe26e63236d362befbb68d28c9e973ae
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-14 16:43:16 +0530

    [api] added
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

--- new-features-react-19_v1/src/main/java/com/java/features/controller/UserDetailsController.java
+++ new-features-react-19_v1/src/main/java/com/java/features/controller/UserDetailsController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.java.features.model.UserDetails;
@@ -21,5 +23,19 @@ public class UserDetailsController {
 		List<UserDetails> userDetails = userService.getUserDetails();
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(userDetails);
+	}
+	
+	@PostMapping(path = "/update-name")
+	public ResponseEntity<String> updateName(@RequestBody UserDetails userDetails) {
+		
+		try {
+			Thread.sleep(5000); // Simulate a delay
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Error updating name: " + e.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK)
+				.body("Name updated successfully");
 	}
 }
