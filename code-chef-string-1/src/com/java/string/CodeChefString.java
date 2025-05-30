8eda0a47fdc54bab82903395fbb927025f8ca2f9
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-30 08:17:49 +0530

    [init] String
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ code-chef-string-1/src/com/java/string/CodeChefString.java
@@ -0,0 +1,18 @@
+package com.java.string;
+
+public class CodeChefString {
+
+	public static void main(String[] args) {
+		String codeChefString = "I Love CodeChef";
+
+		String[] codeChefArray = codeChefString.split(" ");
+		
+		for(String element : codeChefArray) {
+			System.out.println(element);
+		}
+		
+		Math.pow(0, 0);
+		
+	}
+
+}
